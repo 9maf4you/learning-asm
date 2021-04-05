c0bfde81ee7843f40be1dfd71cfa808cea134c82
Maxim <ncx2@yandex.ru>
2021-04-05 10:06:13 +0300

    Update 2.5.s

--- stp/2.5.s
+++ stp/2.5.s
@@ -8,12 +8,12 @@
 
 .text
 _start:
-    mov $1, %rax # номер сискола - это 4 = write
+    mov $1, %rax # номер сискола - это 1 = write
     mov $1, %rdi # fd - куда писать, 1 - стандартный вывод
     mov $msg, %rsi # начало строки Hello, world
     mov $14, %rdx # ee длина
     syscall
 
-    mov $60, %rax
+    mov $60, %rax # syscall exit
     xor %rdi, %rdi
     syscall
